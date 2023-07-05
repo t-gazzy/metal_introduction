511af9c787eb15f5328c36831e2ad8b357bba224
t-gazzy <t.kaji@ntt.com>
2023-07-05 23:47:00 +0900

    feat: add metal handler class

---
+++ src/api_handler.hpp
@@ -0,0 +1,13 @@
+#ifndef API_HANDLER_HPP
+#define API_HANDLER_HPP
+
+namespace metal {
+namespace handler {
+class ApiHandler {
+ public:
+  virtual ~ApiHandler() {};
+};
+}  // namespace handler
+}  // namespace metal
+
+#endif  // API_HANDLER_HPP
---
+++ src/metal_handler.cpp
@@ -0,0 +1,8 @@
+#include "metal_handler.hpp"
+
+namespace metal {
+namespace handler {
+MetalHandler::MetalHandler(/* args */) {}
+MetalHandler::~MetalHandler() {}
+}  // namespace handler
+}  // namespace metal
